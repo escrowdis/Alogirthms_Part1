63f890b03fa393d89a3c3c400c32478dd30f7f9a
escrowdis <eqttonytony@gmail.com>
2014-04-21 16:31:16 +0800

    Create Subset.java

---
+++ Week2/Subset.java
@@ -0,0 +1,29 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tony
+ */
+public class Subset {
+
+    public static void main(String[] args) {
+        int K = Integer.valueOf(args[0]);
+//        Deque<String> dq = new Deque<>();
+        RandomizedQueue rq = new RandomizedQueue();
+        while (StdIn.hasNextLine() && !StdIn.isEmpty()) {
+//            dq.addLast(StdIn.readString());
+            rq.enqueue(StdIn.readString());
+        }
+        
+        for (int i=0; i<K; i++) {
+//            StdOut.print(dq.iterator().next() + " ");
+            StdOut.print(rq.iterator().next());
+        }
+    }
+}
